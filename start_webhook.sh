0dcfc404a5746b542f3d9d39ce5a47096f29ac35
Davi Leichsenring <davileichs@gmail.com>
2025-08-02 15:12:36 +0200

    Update start_webhook.sh

--- start_webhook.sh
+++ start_webhook.sh
@@ -10,6 +10,6 @@ pkill -f "$SCRIPT"
 sleep 2
 
 # Start bots in background with separate logs
-nohup python3 "./$SCRIPT" >> logs/webhook.log 2>&1
+nohup python3 "./$SCRIPT" >> logs/server.log 2>&1
 
 echo "Server started and running in background."
