157c66bbd513fb06014f5a116b4b3852a94db632
Davi Leichsenring <davileichs@gmail.com>
2025-08-03 12:43:01 +0200

    Update start_webhook.sh

--- start_webhook.sh
+++ start_webhook.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-# Define the script names
 SCRIPT="webhook.py"
 
-# Function to start the webhook
 start_webhook() {
     echo "Starting webhook server..."
-    # Kill any existing processes
     pkill -f "$SCRIPT"
     
-    # Short pause
     sleep 2
-    
-    # Start bots in background with separate logs
+
     nohup python3 "./$SCRIPT" >> logs/server.log 2>&1
     
     echo "Server started and running in background."
 }
 
-# Function to restart the webhook
 restart_webhook() {
     echo "Restarting webhook Docker container..."
     
-    # Restart the Docker container using docker-compose
     docker-compose restart
     
     echo "Docker container restarted successfully."
 }
 
-# Check command line arguments
 if [ "$1" = "restart" ]; then
     restart_webhook
 else
