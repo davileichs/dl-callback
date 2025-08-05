4336a309ffb430c5239112e6a712378bc433b67b
Davi Leichsenring <davileichs@gmail.com>
2025-08-05 11:07:27 +0200

    Update start_webhook.sh

--- start_webhook.sh
+++ start_webhook.sh
@@ -251,28 +251,3 @@ case "${1:-start}" in
         exit 1
         ;;
 esac
-
-start_webhook() {
-    echo "Starting webhook server..."
-    pkill -f "$SCRIPT"
-    
-    sleep 2
-
-    nohup python3 "./$SCRIPT" >> logs/server.log 2>&1
-    
-    echo "Server started and running in background."
-}
-
-restart_webhook() {
-    echo "Restarting webhook Docker container..."
-    
-    docker-compose restart
-    
-    echo "Docker container restarted successfully."
-}
-
-if [ "$1" = "restart" ]; then
-    restart_webhook
-else
-    start_webhook
-fi
